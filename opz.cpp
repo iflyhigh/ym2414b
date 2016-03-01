0c27528c6652f5c7e1e50e2267ef8edcac71060e
Andrew Evdokimov <i@iflyhigh.ru>
2016-03-01 10:18:07 +0300

    wip
    volume table now uses 0-127 range instead of 0-99

--- opz.cpp
+++ opz.cpp
@@ -69,7 +69,7 @@ void setreg(uint8_t reg, uint8_t data)
 	//wait(60);
 }
 
-uint8_t tl(uint8_t op, uint8_t vmem_tl, uint8_t vmem_alg, uint8_t vmem_kvs, uint8_t vmem_kls, uint8_t seq_note, uint8_t seq_velocity, uint8_t opz_volume)
+uint8_t tl(uint8_t op, uint8_t vmem_tl, uint8_t vmem_alg, uint8_t vmem_kvs, uint8_t vmem_kls, uint8_t seq_note, uint8_t seq_velocity, uint8_t midi_volume)
 {
 	uint16_t _tl;
 	uint8_t _kvs = 0;
@@ -198,7 +198,7 @@ uint8_t set_note(uint8_t channel, int16_t midi_note, uint8_t midi_velocity, uint
 	uint8_t opz_octave;
 	uint8_t opz_note;
 	uint8_t opz_fraction = 0x00;	// no microtuning at this time
-	uint8_t opz_volume = (uint8_t)round(((float)midi_volume) / 1.283f);
+	//uint8_t opz_volume = (uint8_t)round(((float)midi_volume) / 1.283f);
 
 	midi_note = midi_note + (voice.transpose - 24) - 12;	// TRPS = transpose according to middle C, shift by 12 down for simpler calculations
 
@@ -213,7 +213,7 @@ uint8_t set_note(uint8_t channel, int16_t midi_note, uint8_t midi_velocity, uint
 		for (uint8_t k = 0; k < 4; k++)
 		{
 			setreg(0x60 + channel + 0x08 * k, ( tl(k, voice.op[k].out, (voice.sy_fbl_alg & 0x07), 		// TL = Operator output level
-			                                       (voice.op[k].ame_ebs_kvs & 0x07), voice.op[k].kls, midi_note, midi_velocity, opz_volume)));
+			                                       (voice.op[k].ame_ebs_kvs & 0x07), voice.op[k].kls, midi_note, midi_velocity, midi_volume)));
 		}
 
 		setreg(0x28 + channel, (opz_octave << 4) | opz_note);				// Set channel note
